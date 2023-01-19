df8c7d1da8a266c928a52d4f28737476edcd2bf9
aristrisnawan <aristrisnawan35@gmail.com>
2023-01-19 21:55:12 +0700

    set up asset & color

---
+++ lib/config/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+
+class AppColor {
+  static const primary = Color(0xff767AE7);
+  static const secondary = Color(0xff89FFF0);
+  static const bg = Color(0xff9AB3F5);
+  static const chart = Color(0xffA3D8F4);
+}
