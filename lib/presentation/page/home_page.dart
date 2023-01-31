f31e5440b62680c9b348a84296dec43c683b41cf
aristrisnawan <aristrisnawan35@gmail.com>
2023-01-31 13:40:50 +0700

    add baner in home page

--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:money_record/config/app_color.dart';
 import 'package:money_record/config/session.dart';
 import 'package:money_record/presentation/page/auth/login_page.dart';
 
@@ -9,17 +10,160 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Home Page"),
-            InkWell(
-              onTap: (){
-                Session.deleteUser();
-                Get.off(()=>LoginPage());
-              },
-              child: Icon(Icons.logout))
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80',
+                          width: 50,
+                          height: 50,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Hi,",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text("Indra",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20))
+                        ],
+                      )
+                    ],
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: AppColor.secondary),
+                    child: Center(
+                      child: Icon(
+                        Icons.menu,
+                        size: 40,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            //NOTE : Pengeluran hari ini
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text(
+                "Pengeluaran Hari Ini",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 11,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 167,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColor.primary),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 24,
+                      ),
+                      Text(
+                        "Rp 500.000,00",
+                        style: TextStyle(
+                            color: AppColor.secondary,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "+20% dibanding kemarin",
+                        style: TextStyle(
+                            color: AppColor.secondary,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      SizedBox(
+                        height: 27,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 33,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                bottomLeft: Radius.circular(10))),
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 12),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "Selengkapnya",
+                                style: TextStyle(
+                                    color: AppColor.primary,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                              SizedBox(
+                                width: 7,
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                size: 15,
+                                color: AppColor.primary,
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+            // Text("Home Page"),
+            // InkWell(
+            //     onTap: () {
+            //       Session.deleteUser();
+            //       Get.off(() => LoginPage());
+            //     },
+            //     child: Icon(Icons.logout))
           ],
         ),
       ),
